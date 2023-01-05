dad8aeeef8dd0dba84ea0d7691057e645f0c564b
Heizi <xm.zhongjiaxin@gmail.com>
2023-01-06 01:27:54 +0800

    fix config

--- setting.php
+++ setting.php
@@ -1,22 +1,17 @@
 <?php !defined('DEBUG') AND exit('Access Denied.');
-
+include_once _include(APP_PATH."plugin/sl_repeat_follow/utils/conf.php");
 
 if($method == 'GET') {
-    $setting['sl_repeat_follow_color'] = setting_get('sl_repeat_follow_color');
-    $setting['sl_repeat_follow_perpage'] = setting_get('sl_repeat_follow_perpage');
-    $setting['sl_repeat_follow_b_c'] = setting_get('sl_repeat_follow_b_c');
-    $setting['sl_repeat_follow_b_t'] = setting_get('sl_repeat_follow_b_t');
-    $setting['sl_repeat_follow_b_w'] = setting_get('sl_repeat_follow_b_w');
-    $setting['sl_repeat_follow_b_mw'] = setting_get('sl_repeat_follow_b_mw');
-    include _include(APP_PATH.'plugin/sl_repeat_follow/setting.htm');
+    include_once _include(APP_PATH.'plugin/sl_repeat_follow/setting.phtml');
 } else {
-    setting_set('sl_repeat_follow_color', param('sl_repeat_follow_color', '', FALSE));
-    setting_set('sl_repeat_follow_perpage', param('sl_repeat_follow_perpage', '', FALSE));
-    setting_set('sl_repeat_follow_b_c', param('sl_repeat_follow_b_c', '', FALSE));
-    setting_set('sl_repeat_follow_b_t', param('sl_repeat_follow_b_t', '', FALSE));
-    setting_set('sl_repeat_follow_b_w', param('sl_repeat_follow_b_w', '', FALSE));
-    setting_set('sl_repeat_follow_b_mw', param('sl_repeat_follow_b_mw', '', FALSE));
-    message(0, '修改成功');
+    $r = setConfig([
+        backgroundColor => param('color'),
+        peerPage => param('ppg'),
+        lineColor => param('b_c'),
+        lineStyle => param('b_t'),
+        width => param('b_w'),
+        minWidth => param('b_mw'),
+    ]) ? 0 : 1 ;
+    message(-1, "修改". $r = $r == 0 ? "成功": "失败");
+    echo "<script>alert('$r')</script>";
 }
-
-?>
--- utils/conf.php
+++ utils/conf.php
@@ -1,1 +1,34 @@
 <?php
+
+const backgroundColor = "bgColor";
+const peerPage = "peerPage";
+const width = "width";
+const minWidth = "minWidth";
+const lineStyle = "line";
+const lineColor = "lnColor";
+
+function getConfig(): array {
+    $conf = kv_get("sl_follow_repeat_json_conf");
+    if (!empty($conf)) return json_decode($conf);
+    return [
+        backgroundColor=> setting_get('sl_repeat_follow_color') ?? "#082f3e",
+        peerPage=> setting_get('sl_repeat_follow_perpage') ?? 10,
+        width=> setting_get("sl_repeat_follow_b_w") ?? 80,
+        minWidth=> setting_get("sl_repeat_follow_b_mw") ?? 200,
+        lineStyle=> setting_get("sl_repeat_follow_b_t") ?? 0,
+        lineColor=> setting_get("sl_repeat_follow_b_c") ?? "#082f3e"
+    ];
+}
+function getConfValueOrDefault(array $conf,string $key) {
+    return $conf[$key] ?? getConfig()[$key];
+}
+function setConfig(array $conf):bool {
+    return kv_set("sl_follow_repeat_json_conf",[
+        backgroundColor=> getConfValueOrDefault($conf,backgroundColor),
+        peerPage=> getConfValueOrDefault($conf,peerPage),
+        width=> getConfValueOrDefault($conf,width),
+        minWidth=> getConfValueOrDefault($conf,minWidth),
+        lineStyle=> getConfValueOrDefault($conf,lineStyle),
+        lineColor=> getConfValueOrDefault($conf,lineColor),
+    ]);
+}
