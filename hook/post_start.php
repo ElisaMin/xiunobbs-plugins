6d2d8dc84f7a3e9c222b6ebac26f1828ddaf94f7
Heizi <xm.zhongjiaxin@gmail.com>
2023-01-06 18:19:04 +0800

    rename post to comment

--- hook/post_start.php
+++ hook/post_start.php
@@ -1,25 +1,25 @@
 ?><?php
 if($action == 'rfloor') {
     $pid = param(2);
-    $pageno = param('pageno', 0);
+    $comment = post_read($pid);
+    empty($comment) AND message(-1, lang('post_not_exists'));
     $delfloor = param('delfloor', false);
-    $post = post_read($pid);
-    empty($post) AND message(-1, lang('post_not_exists'));
+
+    $pageno = param('pageno', 0);
+
     $m_s=',';
-    if(empty($post['repeat_follow'])) {
+    if(empty($comment['repeat_follow'])) {
         $m_s=$repeat_follows='';
         if($pageno>0) message(-1, lang('post_not_exists'));
     }
-    else $repeat_follows=$post['repeat_follow'];
-    if($delfloor!==false)
-    {
+    else $repeat_follows=$comment['repeat_follow'];
+    if($delfloor!==false)  {
         empty($repeat_follows) AND message(-1, lang('post_not_exists'));
         $arrs=json_decode($repeat_follows,true);
         $n=0;
         $m_s=$message_json='';
         foreach($arrs as $arr){
-            if($arr['fl']!=$delfloor)
-            {
+            if($arr['fl']!=$delfloor) {
                 if($n>0) $m_s=',';
                 $message_json.=$m_s.'{"fl":"'.$arr['fl'].'","uid":"'.$arr['uid'].'","username":"'.$arr['username'].'","avatar_url":"'.$arr['avatar_url'].'","t_uid":"'.$arr['t_uid'].'","t_username":"'.$arr['t_username'].'","message":"'.str_replace(array('"','\\',),array('\"','\\'.'\\'),$arr['message']).'","update":"'.$arr['update'].'"}';
                 $n++;
@@ -35,21 +35,21 @@ if($action == 'rfloor') {
     }
     $repeat_follows=substr($repeat_follows,1,-1);
     empty($repeat_follows) AND $m_s=$repeat_follows='';
-    $tid = $post['tid'];
-    $thread['uid']=$post['uid'];
-    $thread['subject']=$post['message_fmt'];
-    $thread['tid']=$post['tid'];
+    $tid = $comment['tid'];
+    $thread['uid']=$comment['uid'];
+    $thread['subject']=$comment['message_fmt'];
+    $thread['tid']=$comment['tid'];
     if($pageno>0)
     {
         $return_message='';
         $r_f_g=setting_get('sl_repeat_follow_perpage');
-        $pageno=min($pageno,$post['r_f_c']);
+        $pageno=min($pageno,$comment['r_f_c']);
         $pageno=max($pageno,1);
-        $repeat_follows=json_decode($post['repeat_follow'], true);
+        $repeat_follows=json_decode($comment['repeat_follow'], true);
         $repeat_follows=array_slice($repeat_follows,($pageno-1)*$r_f_g,$r_f_g);
         $message_t=$deltag='';
         foreach($repeat_follows as $repeat_follow){
-            if($repeat_follow['uid']==$uid || $post['floormanage']) $deltag='<a href="javascript:delrfloor('.$pid.',\''.$repeat_follow['fl'].'\');" class="post_update mr-2">删除</a>';
+            if($repeat_follow['uid']==$uid || $comment['floormanage']) $deltag='<a href="javascript:delrfloor('.$pid.',\''.$repeat_follow['fl'].'\');" class="post_update mr-2">删除</a>';
             if($repeat_follow['t_uid']>0 && $repeat_follow['t_username']!='') $message_t='回复 <a href="'.url("user-".$repeat_follow['t_uid']).'" class="text-muted font-weight-bold">'.$repeat_follow['t_username'].'</a>: ';
             $return_message.='<dd class="text-left media" id="pf_'.$pid.'_'.$repeat_follow['fl'].'"><a href="'.url("user-".$repeat_follow['uid']).'" class="mr-2"><img class="avatar-3" onerror="this.src=\'view/img/avatar.png\'"  src="'.$repeat_follow['avatar_url'].'"></a><div style="width:100%;"><span class="text-left"><a href="'.url("user-".$repeat_follow['uid']).'" class="text-muted font-weight-bold">'.$repeat_follow['username'].'</a>: '.$message_t.$repeat_follow['message'].'</span><div class="text-muted text-right">'.$deltag.humandate($repeat_follow['update']).'<a href="javascript:showform('.$pid.',\''.$repeat_follow['username'].'\');" class="post_update ml-2">回复</a></div></div></dd>';
             $message_t=$deltag='';
@@ -82,8 +82,8 @@ if($action == 'rfloor') {
         $recvuid = $thread['uid'];
         notice_send($uid, $recvuid, $notice_message, 2);
     }
-    $r_f_c=$post['r_f_c']+1;
-    $r_f_a=$post['r_f_a']+1;
+    $r_f_c=$comment['r_f_c']+1;
+    $r_f_a=$comment['r_f_a']+1;
     $return_message='<dd class="text-left media" id="pf_'.$pid.'_'.$r_f_a.'"><a href="'.url("user-".$uid).'" class="mr-2"><img class="avatar-3" onerror="this.src=\'view/img/avatar.png\'"  src="'.$user['avatar_url'].'"></a><div style="width:100%;"><span class="text-left"><a href="'.url("user-".$uid).'" class="text-muted font-weight-bold">'.$user['username'].'</a>: '.$message_t.$message.'</span><div class="text-muted text-right"><a href="javascript:delrfloor('.$pid.',\''.$r_f_a.'\');" class="post_update mr-2">删除</a>'.humandate($time).'<a href="javascript:showform('.$pid.',\''.$user['username'].'\');" class="post_update ml-2">回复</a></div></div></dd>';
     $dir = substr(sprintf("%09d", $user['uid']), 0, 3);
     $user_face=$conf['upload_url']."avatar/$dir/$uid.png";
